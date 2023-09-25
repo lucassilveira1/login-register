6a4e47f530d9841051914dfadfda22c526a38845
Lucas Silveira <lucas.silveira0516@gmail.com>
2023-09-25 02:05:34 -0300

    adicionando validações e ícones

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,8 +1,25 @@
-const cpf = document.getElementById('cpf');
 const password = document.getElementById('password');
 const confirm_password = document.getElementById('confirm-password');
-const icon = document.getElementById('icone');
-    
+const showPassword = document.getElementById('showPassword')
+
+const passIcon = document.getElementById("passIcon")
+const confirmPassIcon = document.getElementById("confirmPassIcon")
+const showIcon = document.getElementById("showIcon");
+
+
+const showPassOpen = () => {
+    passIcon.innerHTML = "<img src='/assets/lock-open-solid.svg' width='15px'>"
+    confirmPassIcon.innerHTML = "<img src='/assets/lock-open-solid.svg' width='15px'>"
+    showIcon.innerHTML = "<img src='/assets/eye-slash-regular.svg' width='15px'>"
+};
+
+const notShowPass = () => {
+    passIcon.innerHTML = "<img src='/assets/lock-solid.svg' width='15px'>"
+    confirmPassIcon.innerHTML = "<img src='/assets/lock-solid.svg' width='15px'>"
+    showIcon.innerHTML = "<img src='/assets/eye-regular.svg' width='15px'>"
+};
+
+
 
 // Validando a senha
 function validatePassword() {
@@ -16,11 +33,20 @@ password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
 
 
-// fodase
+// mostrando senha e mudando icones
 
-function clique() {
-
-    window.alert('TATU SUPREMACY');
-
-}
+showPassword.addEventListener('change', function() {
+    if (showPassword.checked) {
+      password.type = 'text';
+      confirm_password.type = 'text'; 
+        // icones
+      showPassOpen()
+    } else {
+      password.type = 'password';
+      confirm_password.type = 'password';
+      
+      // icones
+      notShowPass()
+    }
+});
 
--- js/login.js
+++ js/login.js
@@ -1,9 +1,33 @@
 const login = document.getElementById('login');
 const password = document.getElementById('password');
 const resetBTN = document.getElementById('reset');
+const showPassword = document.getElementById("showPass")
+
+const passwordIcon = document.getElementById("passIcon")
+const checkIcon = document.getElementById("showPassIcon")
+
+const click = () => {
+    passwordIcon.innerHTML = "<img src='/assets/lock-open-solid.svg' width='15px'>"
+    checkIcon.innerHTML = "<img src='/assets/eye-slash-regular.svg' width='15px'>"
+}
+
+const closed = () => {
+    passwordIcon.innerHTML = "<img src='/assets/lock-solid.svg' width='14px'>"
+    checkIcon.innerHTML = "<img src='/assets/eye-regular.svg' width='15px'>"
+}
 
 resetBTN.addEventListener('click', function(e) {
     e.preventDefault();
     login.value = '';
     password.value = '';
 }, false)
+
+showPassword.addEventListener('change', function() {
+    if (showPassword.checked) {
+        password.type = 'text'
+        click()
+    } else {
+        password.type = 'password'
+        closed()
+    }
+})
